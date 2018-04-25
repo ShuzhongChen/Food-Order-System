2a28c886fa5b79b4e36888b2ad7508e903c32bfa
MengtMa <mengtongma@gmai.com>
2018-04-25 15:09:11 -0700

    Add login with facebook

--- app/src/main/java/com/shuzhongchen/foodordersystem/LogIn.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/LogIn.java
@@ -1,13 +1,61 @@
 package com.shuzhongchen.foodordersystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LogIn extends AppCompatActivity {
+
+    LoginButton fbLoginButton;
+    CallbackManager callbackManager;
+    private static final String EMAIL = "email";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        initControls();
+        loginWithFB();
+
+    }
+
+    private void initControls() {
+        callbackManager = CallbackManager.Factory.create();
+        fbLoginButton = findViewById(R.id.fbLogin);
+        fbLoginButton.setReadPermissions(Arrays.asList(EMAIL));
+    }
+
+    private void loginWithFB() {
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                    //code
+            }
+
+            @Override
+            public void onCancel() {
+                
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
