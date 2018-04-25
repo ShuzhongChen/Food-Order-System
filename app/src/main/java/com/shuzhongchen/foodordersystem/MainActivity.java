897f353dfe45cad7544a835aa1e46f0fe05b5aa6
MengtMa <mengtongma@gmai.com>
2018-04-25 11:23:08 -0700

    Welcome page

--- app/src/main/java/com/shuzhongchen/foodordersystem/MainActivity.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/MainActivity.java
@@ -2,12 +2,26 @@ package com.shuzhongchen.foodordersystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignUp;
+    private Button btnSignIn;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+
+
+
     }
 }
