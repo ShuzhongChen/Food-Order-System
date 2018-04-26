cd942673cf591910c5e623ac93cb964fecdcae71
jinchengcheng <15671675579@163.com>
2018-04-26 16:53:19 -0700

    add user login type

--- app/src/main/java/com/shuzhongchen/foodordersystem/LogIn.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/LogIn.java
@@ -1,8 +1,13 @@
 package com.shuzhongchen.foodordersystem;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.facebook.CallbackManager;
@@ -18,6 +23,10 @@ public class LogIn extends AppCompatActivity {
     LoginButton fbLoginButton;
     CallbackManager callbackManager;
     private static final String EMAIL = "email";
+    private boolean customerType = false;
+    private Button btnLogin;
+    private Switch customerTypeSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +35,23 @@ public class LogIn extends AppCompatActivity {
         initControls();
         loginWithFB();
 
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                customerType = customerTypeSwitch.isChecked();
+            }
+
+        });
+
     }
 
     private void initControls() {
         callbackManager = CallbackManager.Factory.create();
         fbLoginButton = findViewById(R.id.fbLogin);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
         fbLoginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        customerTypeSwitch = (Switch)findViewById(R.id.CutomerTypeSwitch);
     }
 
     private void loginWithFB() {
@@ -57,5 +77,6 @@ public class LogIn extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
