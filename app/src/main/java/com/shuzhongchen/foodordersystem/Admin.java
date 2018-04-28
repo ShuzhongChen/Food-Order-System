1f8502d6c145bd9fc13a69d6ba226b960837f69a
jinchengcheng <15671675579@163.com>
2018-04-27 23:55:03 -0700

    add admin dashboard

---
+++ app/src/main/java/com/shuzhongchen/foodordersystem/Admin.java
@@ -0,0 +1,16 @@
+package com.shuzhongchen.foodordersystem;
+
+/**
+ * Created by jinchengcheng on 4/27/18.
+ */
+
+public class Admin {
+
+    public void addMenuItems() {
+
+    }
+
+    public void removeMenuItems() {
+
+    }
+}
