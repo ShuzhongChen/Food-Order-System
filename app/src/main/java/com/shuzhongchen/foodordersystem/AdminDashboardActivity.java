3a79ba09a5220657d737686c12ced368e41c35f6
jinchengcheng <15671675579@163.com>
2018-04-30 15:33:09 -0700

    add menu display

--- app/src/main/java/com/shuzhongchen/foodordersystem/AdminDashboardActivity.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/AdminDashboardActivity.java
@@ -1,5 +1,6 @@
 package com.shuzhongchen.foodordersystem;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * Created by jinchengcheng on 4/27/18.
@@ -15,6 +17,14 @@ import android.widget.ImageView;
 public class AdminDashboardActivity extends AppCompatActivity {
 
     private ImageButton addImage;
+    private ListView listView;
+    String[] menuName = {"test1", "test2", "test3", "test4"};
+    String[] menuCategory = {"category1", "category2", "category3", "category4"};
+    int[] menuCalories = {1,2,3,4};
+    int[] menuUnitPrice = {1,2,3,4};
+    int[] menuPrepTime = {1,2,3,4};
+
+    private Activity content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +33,28 @@ public class AdminDashboardActivity extends AppCompatActivity {
 
         initial();
 
-        addImage.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager manager = getFragmentManager();
-                FragmentTransaction transaction = manager.beginTransaction();
-                transaction.replace(R.id.MainContainer,new addMenuItemFragment());
-                System.out.print("executed replace function");
-                transaction.addToBackStack(null);
-                transaction.commit();
-            }
-        }));
+        listView = (ListView) findViewById(R.id.MenuListView);
+        CustomListView customListView = new CustomListView(this, menuName, menuCategory, menuUnitPrice, menuCalories, menuPrepTime);
+        listView.setAdapter(customListView);
+
+
+//        addImage.setOnClickListener ((new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FragmentManager manager = getFragmentManager();
+//                FragmentTransaction transaction = manager.beginTransaction();
+//                transaction.replace(R.id.MainContainer,new addMenuItemFragment());
+//                System.out.print("executed replace function");
+//                transaction.addToBackStack(null);
+//                transaction.commit();
+//            }
+//        }));
+
 
     }
 
     private void initial() {
-        addImage = (ImageButton) findViewById(R.id.addImageButton);
+        //addImage = (ImageButton) findViewById(R.id.addImageButton);
     }
 
 }
---
+++ app/src/main/java/com/shuzhongchen/foodordersystem/CustomListView.java
@@ -0,0 +1,85 @@
+package com.shuzhongchen.foodordersystem;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jinchengcheng on 4/29/18.
+ */
+
+public class CustomListView extends ArrayAdapter<String>{
+
+    private Context context;
+    private String[] listOfName;
+    private String[] listOfCategory;
+    private int[] listOfCalories;
+    private int[] listOfUnitPrice;
+    private int[] listOfPrepTime;
+
+    public CustomListView(@NonNull Context context,String[] listOfName, String[] listOfCategory,
+                          int[] listOfCalories, int[] listOfUnitPrice, int[] listOfPrepTime) {
+        super(context, R.layout.menu_dispaly, listOfName);
+
+        this.listOfName = listOfName;
+        this.listOfCalories = listOfCalories;
+        this.listOfCategory = listOfCategory;
+        this.listOfUnitPrice = listOfUnitPrice;
+        this.listOfPrepTime = listOfPrepTime;
+
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        viewHolder vh = null;
+
+        if (vh == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            r = layoutInflater.inflate(R.layout.menu_dispaly, null, true);
+
+            vh = new viewHolder(r);
+            r.setTag(1);
+        } else {
+            vh = (viewHolder)r.getTag(1);
+        }
+
+        vh.nameTV.setText(listOfName[position]);
+        vh.caloriesTV.setText(String.valueOf(listOfCalories[position]));
+        vh.categoryTV.setText(listOfCategory[position]);
+        vh.UnitPriceTV.setText(String.valueOf(listOfUnitPrice[position]));
+        vh.PrepTimeTV.setText(String.valueOf(listOfPrepTime[position]));
+
+        return r;
+    }
+
+
+    class viewHolder {
+
+        TextView nameTV;
+        TextView categoryTV;
+        TextView caloriesTV;
+        TextView UnitPriceTV;
+        TextView PrepTimeTV;
+        ImageButton imageButton;
+
+        viewHolder(View view) {
+            nameTV = (TextView)view.findViewById(R.id.menu_name_textView);
+            categoryTV = (TextView)view.findViewById(R.id.menu_category_textview);
+            caloriesTV = (TextView) view.findViewById(R.id.menu_calories_textView);
+            UnitPriceTV = (TextView) view.findViewById(R.id.menu_unit_price_textView);
+            PrepTimeTV = (TextView) view.findViewById(R.id.menu_preptime_textView);
+            imageButton = (ImageButton) view.findViewById(R.id.menu_image);
+        }
+    }
+}
