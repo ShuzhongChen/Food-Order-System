786b1a80dddf979ee1e2f3332501cdb5d0205887
“ShuzhongChen” <chenshuzhongs@gmail.com>
2018-05-08 14:59:06 -0700

    add button to order layout

--- app/src/main/java/com/shuzhongchen/foodordersystem/holders/MenuOrderViewHolder.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/holders/MenuOrderViewHolder.java
@@ -13,7 +13,6 @@ import com.shuzhongchen.foodordersystem.R;
 public class MenuOrderViewHolder extends RecyclerView.ViewHolder {
     public TextView order_food_name;
     public TextView order_food_price;
-    
 
     public int num;
     public String id;
