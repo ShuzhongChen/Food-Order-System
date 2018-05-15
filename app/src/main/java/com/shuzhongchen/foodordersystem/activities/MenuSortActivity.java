288f634f9b1b20c3c6f8700ed4493ddb2f271eda
jinchengcheng <15671675579@163.com>
2018-05-15 11:28:39 -0700

    add logic to date picker

--- app/src/main/java/com/shuzhongchen/foodordersystem/activities/MenuSortActivity.java
+++ app/src/main/java/com/shuzhongchen/foodordersystem/activities/MenuSortActivity.java
@@ -2,15 +2,23 @@ package com.shuzhongchen.foodordersystem.activities;
 
 import android.annotation.TargetApi;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.wifi.aware.Characteristics;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shuzhongchen.foodordersystem.R;
 
@@ -25,20 +33,42 @@ public class MenuSortActivity extends AppCompatActivity {
     TextView startDatePicker;
     TextView endDatePicker;
     Button nextButton;
-    Calendar newCalendar;
+    Calendar myCalendar;
 
     DatePickerDialog datePickerDialog;
+
+
+    int startDay;
+    int startMonth;
+    int startYear;
+
+    boolean startDateChoosed;
+    boolean endDateChoosed;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_choose_date);
         System.out.println("OnCreateView Pressed!" + "\n");
 
-        setContentView(R.layout.admin_choose_date);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         startDatePicker = (TextView) findViewById(R.id.startDate);
         endDatePicker = (TextView) findViewById(R.id.endDate);
         nextButton = (Button) findViewById(R.id.nextButton);
-        newCalendar = Calendar.getInstance();
+        myCalendar = Calendar.getInstance();
+
+        startDay = myCalendar.get(Calendar.DAY_OF_MONTH);
+        startMonth = myCalendar.get(Calendar.MONTH);
+        startYear = myCalendar.get(Calendar.YEAR);
+
+        startDateChoosed = false;
+        endDateChoosed = false;
 
         startDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +76,10 @@ public class MenuSortActivity extends AppCompatActivity {
                 System.out.println("start date picker clicked " + "\n");
 
                 // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+                int mYear = myCalendar.get(Calendar.YEAR); // current year
+                int mMonth = myCalendar.get(Calendar.MONTH); // current month
+                int mDay = myCalendar.get(Calendar.DAY_OF_MONTH); // current day
                 // date picker dialog
                 datePickerDialog = new DatePickerDialog(MenuSortActivity.this, new DatePickerDialog.OnDateSetListener() {
 
@@ -61,6 +91,10 @@ public class MenuSortActivity extends AppCompatActivity {
                          + "year: " + year + "\n");
 
                         startDatePicker.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        startDay = dayOfMonth;
+                        startMonth = monthOfYear;
+                        startYear = year;
+                        startDateChoosed = true;
 
                     }
                 }, mYear, mMonth, mDay);
@@ -81,30 +115,32 @@ public class MenuSortActivity extends AppCompatActivity {
             public void onClick(View view) {
                 System.out.println("start date picker clicked " + "\n");
 
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                datePickerDialog = new DatePickerDialog(MenuSortActivity.this, new DatePickerDialog.OnDateSetListener() {
+                if (startDateChoosed) {
 
-                    @Override
-                    public void onDateSet(DatePicker view, int year,
-                                          int monthOfYear, int dayOfMonth) {
+                    // date picker dialog
+                    datePickerDialog = new DatePickerDialog(MenuSortActivity.this, new DatePickerDialog.OnDateSetListener() {
 
-                        endDatePicker.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
 
-                    }
-                }, mYear, mMonth, mDay);
+                            endDatePicker.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            endDateChoosed = true;
+
+                        }
+                    }, startYear, startMonth, startDay);
 
 
-                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
-//                Calendar cal = Calendar.getInstance();
-//                cal.add(Calendar.DAY_OF_WEEK, +7);
-//                datePickerDialog.getDatePicker().setMaxDate(cal.getTimeInMillis());
+                    datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                    Calendar cal = Calendar.getInstance();
+                    cal.add(Calendar.DAY_OF_WEEK, +7);
+                    datePickerDialog.getDatePicker().setMaxDate(cal.getTimeInMillis());
 
-                datePickerDialog.show();
+                    datePickerDialog.show();
+
+                } else {
+                    Toast.makeText(MenuSortActivity.this, "please choose start date first", Toast.LENGTH_LONG).show();
+                }
             }
 
         });
@@ -113,10 +149,22 @@ public class MenuSortActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // get the values for day of month , month and year from a date picker
+                String startDate = startDatePicker.getText().toString();
+                String endDate = endDatePicker.getText().toString();
 
-                //Toast.makeText(getApplicationContext(), day + "\n" + month + "\n" + year, Toast.LENGTH_LONG).show();
+                String[] s = startDate.split("/");
+                String[] e = endDate.split("/");
+
+                if (!startDateChoosed || !endDateChoosed) {
+                    Toast.makeText(MenuSortActivity.this, "please choose date", Toast.LENGTH_LONG).show();
+                } else {
+                    //go to show the order page;
+                }
+
             }
         });
     }
+
 }
+
+
